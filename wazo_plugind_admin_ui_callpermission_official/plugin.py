b982adebcb45d1a022bb503b78f98b135c67abcb
Cedric Abunar <cabunar@wazo.io>
2018-02-15 13:37:08 -0500

    add call permission form

--- wazo_plugind_admin_ui_callpermission_official/plugin.py
+++ wazo_plugind_admin_ui_callpermission_official/plugin.py
@@ -1,7 +1,13 @@
 # Copyright 2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+from flask_menu.classy import register_flaskview
+
 from wazo_admin_ui.helpers.plugin import create_blueprint
+from wazo_admin_ui.helpers.destination import register_listing_url
+
+from .service import CallpermissionService
+from .view import CallpermissionView, CallpermissionListingView
 
 callpermission = create_blueprint('callpermission', __name__)
 
@@ -10,5 +16,14 @@ class Plugin(object):
 
     def load(self, dependencies):
         core = dependencies['flask']
+
+        CallpermissionView.service = CallpermissionService()
+        CallpermissionView.register(callpermission, route_base='/callpermissions')
+        register_flaskview(callpermission, CallpermissionView)
+
+        CallpermissionListingView.service = CallpermissionService()
+        CallpermissionListingView.register(callpermission, route_base='/callpermissions_listing')
+
+        register_listing_url('callpermission', 'callpermission.CallpermissionListingView:list_json')
 
         core.register_blueprint(callpermission)
---
+++ wazo_plugind_admin_ui_callpermission_official/service.py
@@ -0,0 +1,57 @@
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from wazo_admin_ui.helpers.confd import confd
+from wazo_admin_ui.helpers.service import BaseConfdService
+
+
+class CallpermissionService(BaseConfdService):
+    resource_confd = 'call_permissions'
+
+    def get(self, resource_id):
+        resource = super().get(resource_id)
+        users = confd.call_permissions(resource_id).list_users()
+        resource['users'] = self._build_user_list(users['items'])
+        return resource
+
+    def _build_user_list(self, users):
+        result = []
+        for user in users:
+            user_data = self.get_user(user['user_id'])
+            result.append({
+                'id': user['user_id'],
+                'firstname': user_data['firstname'],
+                'lastname': user_data['lastname']
+            })
+        return result
+
+    def update(self, resource):
+        super().update(resource)
+        existing_resource = self.get(resource['id'])
+        self.update_users(resource['id'], resource['user_ids'], existing_resource['users'])
+        self.update_groups(resource['id'], resource['group_ids'], existing_resource['groups'])
+        self.update_outcals(resource['id'], resource['outcall_ids'], existing_resource['outcalls'])
+
+    def update_users(self, callpermission_id, user_ids, existing_user_ids=None):
+        if existing_user_ids:
+            for existing_user_id in existing_user_ids:
+                confd.users(existing_user_id).remove_call_permission(callpermission_id)
+        for user_id in user_ids:
+            confd.users(user_id).add_call_permission(callpermission_id)
+
+    def update_groups(self, callpermission_id, groups_ids, existing_groups_ids=None):
+        if existing_groups_ids:
+            for existing_group_id in existing_groups_ids:
+                confd.groups(existing_group_id).remove_call_permission(callpermission_id)
+        for groups_id in groups_ids:
+            confd.groups(groups_id).add_call_permission(callpermission_id)
+
+    def update_outcals(self, callpermission_id, outcall_ids, existing_outcall_ids=None):
+        if existing_outcall_ids:
+            for existing_outcall_id in existing_outcall_ids:
+                confd.outcalls(existing_outcall_id).remove_call_permission(callpermission_id)
+        for outcall_id in outcall_ids:
+            confd.outcalls(outcall_id).add_call_permission(callpermission_id)
+
+    def get_user(self, user_id):
+        return confd.users.get(user_id)
