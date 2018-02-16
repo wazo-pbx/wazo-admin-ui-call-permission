93429b57dc41a71dc8fd1fae4166dc215de184a5
Francois Blackburn <fblackburn@wazo.community>
2018-02-16 12:48:37 -0500

    remove unused check

--- wazo_plugind_admin_ui_callpermission_official/service.py
+++ wazo_plugind_admin_ui_callpermission_official/service.py
@@ -32,23 +32,20 @@ class CallPermissionService(BaseConfdService):
         self.update_groups(resource['id'], resource['group_ids'], existing_resource['groups'])
         self.update_outcalls(resource['id'], resource['outcall_ids'], existing_resource['outcalls'])
 
-    def update_users(self, callpermission_id, user_ids, existing_user_ids=None):
-        if existing_user_ids:
-            for existing_user_id in existing_user_ids:
-                confd.users(existing_user_id).remove_call_permission(callpermission_id)
+    def update_users(self, callpermission_id, user_ids, existing_user_ids):
+        for existing_user_id in existing_user_ids:
+            confd.users(existing_user_id).remove_call_permission(callpermission_id)
         for user_id in user_ids:
             confd.users(user_id).add_call_permission(callpermission_id)
 
-    def update_groups(self, callpermission_id, groups_ids, existing_groups_ids=None):
-        if existing_groups_ids:
-            for existing_group_id in existing_groups_ids:
-                confd.groups(existing_group_id).remove_call_permission(callpermission_id)
+    def update_groups(self, callpermission_id, groups_ids, existing_groups_ids):
+        for existing_group_id in existing_groups_ids:
+            confd.groups(existing_group_id).remove_call_permission(callpermission_id)
         for groups_id in groups_ids:
             confd.groups(groups_id).add_call_permission(callpermission_id)
 
-    def update_outcalls(self, callpermission_id, outcall_ids, existing_outcall_ids=None):
-        if existing_outcall_ids:
-            for existing_outcall_id in existing_outcall_ids:
-                confd.outcalls(existing_outcall_id).remove_call_permission(callpermission_id)
+    def update_outcalls(self, callpermission_id, outcall_ids, existing_outcall_ids):
+        for existing_outcall_id in existing_outcall_ids:
+            confd.outcalls(existing_outcall_id).remove_call_permission(callpermission_id)
         for outcall_id in outcall_ids:
             confd.outcalls(outcall_id).add_call_permission(callpermission_id)
